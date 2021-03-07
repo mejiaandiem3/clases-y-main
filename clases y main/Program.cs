ab8561b55099a5ccd3f5b6aaceddd7ebc4275497
Andrea Evelyn Mejía Rubio <andiemartin33@gmail.com>
2021-03-07 02:19:51 -0600

    Agregar archivos de proyecto.

---
+++ clases y main/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clases_y_main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Promedioestudiantes p = new Promedioestudiantes();
+
+            p.prom();
+
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
---
+++ clases y main/Promedioestudiantes.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clases_y_main
+{
+    class Promedioestudiantes
+    {
+
+        int calificacion;
+        int promedio;
+        int suma = 0;
+
+        public void prom()
+        {
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Dame tu calificacion: ");
+                calificacion = Convert.ToInt32(Console.ReadLine());
+
+                suma = calificacion + suma;
+            }
+
+
+            promedio = suma / 5;
+
+            Console.WriteLine("El promedio final es: " + promedio);
+        }
+    }
+}
